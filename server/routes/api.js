a4af1af35a1d5a10a916621bbdaa796a4e1990fb
Sonya Hu <sonyahu15@gmail.com>
2023-10-10 15:29:28 -0400

    setting up supertest

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,29 +1,28 @@
 const express = require('express');
 const pokemonController = require('../controllers/pokemonController');
 const userController = require('../controllers/userController');
-const Pokemon = require('../models/pokemonModels');
-
+const { Pokemon } = require('../models/pokemonModels.js');
 
 const router = express.Router();
-router.use(express.json())
+router.use(express.json());
 
 //Route to retieve random pokemon data using the getpokemon middleware from pokemon controller
 //Returns the json responce containing the random pokemon data
 router.get('/', pokemonController.getPokemon, (req, res) => {
   return res.status(200).json(res.locals.randomPokemon);
-})
+});
 
 // Route to handle user registration using the createUser middleware from userController.
 // Returns a JSON response indicating that the user has been added to the database.
 router.post('/signup', userController.createUser, (req, res) => {
   return res.status(200).json('Added user to the db');
-})
+});
 
 // Route to handle user login using the loginUser middleware from userController.
 // Returns a JSON response indicating that the user is logged in.
 router.post('/login', userController.loginUser, (req, res) => {
   return res.status(200).json('User is logged in');
-})
+});
 
 // This router is designed to populate your database. It looks unconventional, but it does work.
 // Essentially, the router calls our middleware function, fetchPokemonData, to populate an array
@@ -32,17 +31,17 @@ router.post('/login', userController.loginUser, (req, res) => {
 // mongoDB connection URL.
 
 // router.get('/fetch-and-store-pokemons', async (req, res) => {
-//     try {
-//       const pokemonDataList = await pokemonController.fetchPokemonData();
-//       await Pokemon.create(pokemonDataList);
-//       console.log(`Stored ${pokemonDataList.length} Pokémon in the database.`);
-//       res.json({ message: `Stored ${pokemonDataList.length} Pokémon in the database.` });
-//     } catch (error) {
-//       console.error(`Error fetching/storing data for Pokémon: ${error.message}`);
-//       res.status(500).json({ error: 'Internal Server Error' });
-//     }
+//   try {
+//     const pokemonDataList = await pokemonController.fetchPokemonData();
+//     await Pokemon.create(pokemonDataList);
+//     console.log(`Stored ${pokemonDataList.length} Pokémon in the database.`);
+//     res.json({
+//       message: `Stored ${pokemonDataList.length} Pokémon in the database.`,
+//     });
+//   } catch (error) {
+//     console.error(`Error fetching/storing data for Pokémon: ${error.message}`);
+//     res.status(500).json({ error: 'Internal Server Error' });
+//   }
 // });
 
-
-
-  module.exports = router;
+module.exports = router;
