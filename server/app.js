26cefdc8e677582110111c2c72ac982d1c6f7d64
Gabriela Pleitez Gomez <gabspg@gmail.com>
2023-10-11 10:15:34 -0400

    frontend and backend testing environment is set up

---
+++ server/app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express();
+const apiRouter = require('./routes/api');
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static(__dirname + '/public/'));
+
+//set up the router here for '/
+app.use('/pokemon', apiRouter);
+
+// Unknown route handler
+app.use('*', (req, res) => res.status(404));
+
+//Global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+module.exports = app;
--- server/server.js
+++ server/server.js
@@ -1,29 +1,5 @@
-const express = require('express');
-const app = express();
-const apiRouter = require('./routes/api');
+const app = require('./app');
 
 const PORT = 3000;
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(express.static(__dirname + '/public/'));
-
-//set up the router here for '/
-app.use('/pokemon', apiRouter);
-
-// Unknown route handler
-app.use('*', (req, res) => res.status(404));
-
-//Global error handler
-app.use((err, req, res, next) => {
-  const defaultErr = {
-    log: 'Express error handler caught unknown middleware error',
-    status: 500,
-    message: { err: 'An error occurred' },
-  };
-  const errorObj = Object.assign({}, defaultErr, err);
-  console.log(errorObj.log);
-  return res.status(errorObj.status).json(errorObj.message);
-});
 
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
