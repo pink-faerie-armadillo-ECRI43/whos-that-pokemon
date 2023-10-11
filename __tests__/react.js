7d7228926d05b709402b62f4491c77debd000759
henry.sweat <hi@henrysweat.com>
2023-10-10 20:08:14 -0400

    testing

---
+++ __tests__/react.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import { render, screen, waitFor } from '@testing-library/react';
+
+describe('Unit testing React components', () => {
+  test('test', () => {
+    expect(true).toEqual(true);
+  });
+});
