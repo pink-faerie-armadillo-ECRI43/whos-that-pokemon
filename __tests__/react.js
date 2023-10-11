e002a8648aac487ec2478a61f098d54553e40ff6
Corgul <murray.josh7@gmail.com>
2023-10-11 09:52:01 -0400

    working react test

--- __tests__/react.js
+++ __tests__/react.js
@@ -1,8 +1,3 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import userEvent from '@testing-library/user-event';
-import { render, screen, waitFor } from '@testing-library/react';
-
 describe('Unit testing React components', () => {
   test('test', () => {
     expect(true).toEqual(true);
