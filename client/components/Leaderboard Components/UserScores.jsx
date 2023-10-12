d9b5100fe5b09f8a781b658e80113dbccacd277a
Gabriela Pleitez Gomez <gabspg@gmail.com>
2023-10-12 18:01:45 -0400

    leaderboard css

--- client/components/Leaderboard Components/UserScores.jsx
+++ client/components/Leaderboard Components/UserScores.jsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
 const UserScores = ({ number, username, score }) => {
-    // render logic for each user score
-    return (
-        <div className='user-score-box'>
-          <div id="score-user-name">
-            <strong>{number}{' - '}{username}</strong>
-          </div>
-          <div id="score-score">
-            <strong>{score}</strong>
-          </div>
-        </div>
-    );
+  // render logic for each user score
+  let name = username;
+  if (username && username !== 'Username') {
+    name = name.toUpperCase();
+  }
+  return (
+    <div className='user-score-box'>
+      <div id='rank'>
+        <strong>{number}</strong>
+      </div>
+      <div id='score-user-name'>
+        <strong>{name}</strong>
+      </div>
+      <div id='score-score'>
+        <strong>{score}</strong>
+      </div>
+    </div>
+  );
 };
 
 export default UserScores;
